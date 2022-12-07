45c3f3db88ec1d7819da33dedbf89c732b1f4ca9
Gmzest <107549476+Gmzest@users.noreply.github.com>
2022-12-07 12:43:03 +0300

    Update main.py

--- Лабораторная работа 2/Класс Book/main.py
+++ Лабораторная работа 2/Класс Book/main.py
@@ -23,7 +23,7 @@ class Book:
         return f'Книга "{self.name}"'
 
     def __repr__(self) -> str:
-        return f"Book(id_={self.id}, name='{self.name}', pages={self.pages})"
+        return f"Book(id_={self.id}, name={self.name!r}, pages={self.pages})"
 
 
 if __name__ == '__main__':
